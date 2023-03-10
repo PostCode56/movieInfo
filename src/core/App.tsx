02c8271031ddab1bd138348e72f9c32ab9702068
PostCode56 <hipgit@inbox.ru>
2023-03-10 16:51:26 +0300

    test

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,16 +1,16 @@
 import React, {FC} from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import {Error, Main, InfoCardMovie} from "../components";
 
 const App: FC = () => {
     return (
-        <BrowserRouter basename="/movieInfo">
+        <HashRouter>
             <Routes>
-                <Route path="/" element={<Main/>}/>
-                <Route path="/info/:id" element={<InfoCardMovie/>}/>
-                <Route path='/*' element={<Error/>}/>
+                <Route path="" element={<Main/>}/>
+                <Route path="info/:id" element={<InfoCardMovie/>}/>
+                <Route path='*' element={<Error/>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 };
 
